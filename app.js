19fa8b2d43dc02ebaed1cf6d7fe93366d689d11a
Touhidul Islam Chayan <imchn24@gmail.com>
2020-09-01 18:17:29 +0600

    mlab added

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+const port = process.env.PORT || 8080;
 var methodOverride = require('method-override');
 var expressSanitizer = require('express-sanitizer');
-const { render } = require('ejs');
-const { request } = require('express');
+const {
+    render
+} = require('ejs');
+const {
+    request
+} = require('express');
 
 var app = express();
-mongoose.connect("mongodb://localhost/thotsApp",{useNewUrlParser: true, useUnifiedTopology: true});
+/* mongoose.connect("mongodb://localhost/thotsApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}); */
+mongoose.connect("mongodb+srv://chnspart:maxchayan17@yourthot.w6eph.mongodb.net/yourthot?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 //App config
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 //Mongoose-Schema
@@ -19,7 +34,10 @@ var thotSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now}
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
 //Mongoose-Model
 var Thots = mongoose.model("Thots", thotSchema);
@@ -32,75 +50,81 @@ var Thots = mongoose.model("Thots", thotSchema);
 //Routes
 
 /***********************index*************************/
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.redirect("/thots");
 });
-app.get("/thots", function(req, res){
-    Thots.find({}, function(err, foundThot){
-        if(err){
+app.get("/thots", function (req, res) {
+    Thots.find({}, function (err, foundThot) {
+        if (err) {
             console.log("ERROR!");
-        }else{
-            res.render("index", {thots : foundThot});
+        } else {
+            res.render("index", {
+                thots: foundThot
+            });
         }
     });
 });
 /***********************new route*********************/
-app.get("/thots/new", function(req, res){
+app.get("/thots/new", function (req, res) {
     res.render("new");
 });
 /*********************create route*******************/
-app.post("/thots", function(req, res){
+app.post("/thots", function (req, res) {
     req.body.thot.body = req.sanitize(req.body.thot.body);
-    Thots.create(req.body.thot, function(err, newThot){
-        if(err){
+    Thots.create(req.body.thot, function (err, newThot) {
+        if (err) {
             console.log("ERROR!");
-        }else{
+        } else {
             res.redirect("/thots");
         }
     });
 });
 /*********************show route*********************/
-app.get("/thots/:id", function(req, res){
-    Thots.findById(req.params.id, function(err, foundThot){
-        if(err){
+app.get("/thots/:id", function (req, res) {
+    Thots.findById(req.params.id, function (err, foundThot) {
+        if (err) {
             res.redirect("/thots");
-        }else{
-            res.render("show", {thot:foundThot});
+        } else {
+            res.render("show", {
+                thot: foundThot
+            });
         }
     });
 });
 /*********************edit route*********************/
-app.get("/thots/:id/edit", function(req, res){
-    Thots.findById(req.params.id, function(err, foundThot){
-        if(err){
+app.get("/thots/:id/edit", function (req, res) {
+    Thots.findById(req.params.id, function (err, foundThot) {
+        if (err) {
             res.redirect("/thots");
-        }else{
-            res.render("edit", {thot:foundThot});
+        } else {
+            res.render("edit", {
+                thot: foundThot
+            });
         }
     });
 });
 /*********************update route*********************/
-app.put("/thots/:id", function(req, res){
+app.put("/thots/:id", function (req, res) {
     req.body.thot.body = req.sanitize(req.body.thot.body);
-    Thots.findByIdAndUpdate(req.params.id, req.body.thot, function(err, updatedThot){
-        if(err){
+    Thots.findByIdAndUpdate(req.params.id, req.body.thot, function (err, updatedThot) {
+        if (err) {
             res.redirect("/thots");
-        }else{
+        } else {
             res.redirect("/thots/" + req.params.id);
         }
     });
 });
 /*********************delete route*********************/
-app.delete("/thots/:id", function(req, res){
-    Thots.findByIdAndRemove(req.params.id, function(err){
-        if(err){
+app.delete("/thots/:id", function (req, res) {
+    Thots.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
             res.redirect("/thots");
-        }else{
+        } else {
             res.redirect("/thots");
         }
     });
 });
 
-app.listen(8080, function(){
+app.listen(port, function () {
     console.log("Thot begones....///");
 });
